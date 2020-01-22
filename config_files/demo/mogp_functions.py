10a51743c1176bf7f472f04b2203261e3145aa54
edaub <edaub@turing.ac.uk>
2020-01-22 15:20:33 +0000

    cleaned up functions to make accessible to users

--- config_files/demo/mogp_functions.py
+++ config_files/demo/mogp_functions.py
@@ -12,9 +12,7 @@ except ModuleNotFoundError:
     import pickle
 
 
-def run_mogp_simulation(mpi_exec, fdfault_exec, results_dir):
-
-    np.random.seed(157374)
+def run_fdfault_simulation(mpi_exec, fdfault_exec, results_dir):
 
     input_points = np.load(join(results_dir, "input_points.npy"))
 
@@ -42,9 +40,7 @@ def run_mogp_simulation(mpi_exec, fdfault_exec, results_dir):
     np.save('input_points.npy', input_points)
 
 
-def run_mogp_analysis(mpi_exec, fdfault_exec, results_dir):
-
-    np.random.seed(157374)
+def load_results(results_dir):
 
     ed = None
     input_points = []
@@ -66,60 +62,32 @@ def run_mogp_analysis(mpi_exec, fdfault_exec, results_dir):
     input_points = np.array(input_points)
     results = np.array(results)
 
-    # Now fit a Gaussian Process to the input_points and results to fit the
-    # approximate model. We use the basic maximum martinal likelihood method
-    # to estimate the GP hyperparameters
+    return input_points, results, ed
+
+def run_mogp_analysis(analysis_points, known_value, threshold, results_dir):
+
+    input_points, results, ed = load_results(results_dir)
+
+    # fit GP to simulations
+
     gp = mogp_emulator.GaussianProcess(input_points, results)
     gp.learn_hyperparameters()
 
     # We can now make predictions for a large number of input points much
     # more quickly than running the simulation.
-    query_points = ed.sample(1000)
+
+    query_points = ed.sample(analysis_points)
     predictions = gp.predict(query_points)
 
-    # predictions contains both the mean values and variances from the
-    # approximate model, so we can use this to quantify uncertainty given
-    # a known value of the moment.
-    # Since I don't have an actual observation to use, I will do a synthetic
-    # test by running an additional point so I can evaluate the results from
-    # the known inputs.
-    if not exists(join(results_dir, "data")):
-        makedirs(join(results_dir, "data"))
-    if not exists(join(results_dir, "problems")):
-        makedirs(join(results_dir, "problems"))
+    # set up history matching
 
-    known_input = ed.sample(1)
-    name = "known_value"
-    create_problem(known_input[0], name=name, output_dir=results_dir)
-    run_simulation(name=name, n_proc=4, mpi_exec=mpi_exec,
-                   fdfault_exec=fdfault_exec, output_dir=results_dir)
-    known_value = compute_moment(name=name, results_dir=results_dir)
-
-    # One easy method for comparing a model with observations is known as
-    # History Matching, where you compute an implausibility measure for many
-    # sample points given all sources of uncertainty (observational error,
-    # approximate model uncertainty, and "model discrepancy" which is a measure
-    # of how good the model is at describing reality). For simplicity here we
-    # will only consider the approximate model uncertainty, but for real
-    # situations it is important to include all three sources.
-    # The implausibility is then just the number of standard deviations between
-    # the predicted value and the known value.
-
-    # To compute the implausibility, we use the HistoryMatching class, which
-    # requires the observation, query points (coords), and predicted values
-    # (expectations), plus a threshold above which we can rule out a point
-
-    hm = mogp_emulator.HistoryMatching(obs=known_value, coords=query_points,
-                                       expectations=predictions, threshold=2.)
+    hm = mogp_emulator.HistoryMatching(obs=known_value, expectations=predictions,
+                                       threshold=threshold)
 
     implaus = hm.get_implausibility()
+    NROY = hm.get_NROY()
 
-    # We can see which points have not been ruled out yet (NROY) based on the
-    # implausibility threshold.
-
-    print("Actual point:", known_input[0])
-    print("NROY:")
-    print(query_points[hm.get_NROY()])
+    # make some plots
 
 
 if __name__ == "__main__":
@@ -137,10 +105,24 @@ if __name__ == "__main__":
             print("Error : input sample points should be integer value !")
             exit()
 
-        run_mogp_simulation(mpi_exec, fdfault_exec, results_dir)
-    elif mood == "analysis":
-        mpi_exec = sys.argv[2]
-        fdfault_exec = sys.argv[3]
-        results_dir = sys.argv[4]
+        run_fdfault_simulation(mpi_exec, fdfault_exec, results_dir)
 
-        run_mogp_analysis(mpi_exec, fdfault_exec, results_dir)
+    elif mood == "analysis":
+        try:
+            analysis_points = int(sys.argv[2])
+        except ValueError:
+            print("Error: number of analysis points must be an integer")
+            exit()
+        try:
+            known_value = float(sys.argv[3])
+        except ValueError:
+            print("Error: known value must be a float")
+            exit()
+        try:
+            threshold = float(sys.argv[4])
+        except ValueError:
+            print("Error: threshold must be a float")
+            exit()
+        results_dir = sys.argv[5]
+
+        run_mogp_analysis(analysis_points, known_value, threshold, results_dir)
